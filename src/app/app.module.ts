d8a9ef2b9cc243188d81a658f546435c9429ab6f
allenlr <allenlru55@gmail.com>
2024-01-02 02:12:39 -0600

    add app-shell to app.component.html, replace individual components from app.module.ts with sharedmodule, add styling from source repo to shell.component.scss, build app layout in shell.component.html, use breakpoint, handset shareplay in shellcomponent, change development environments.ts to production: false

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatButtonModule } from "@angular/material/button"
 import { SharedModule } from './shared/shared.module';
-import { ShellComponent } from './shell/shell.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShellComponent
   ],
   imports: [
     BrowserModule,
